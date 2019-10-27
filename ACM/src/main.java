a124616ca233ba4c97bf944a2e45bf32b5911c59
Yousef Ahmed <32042409+YosefAhmed@users.noreply.github.com>
2019-10-27 17:49:37 +0200

    acm

---
+++ ACM/src/main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class main {
+
+	public static void main(String [] args) {
+		Scanner input=new Scanner(System.in);
+		String name=input.nextLine();
+		String[] myname ;
+		myname=name.split(name, 2);
+		for(int i=0;i<name.length();i++) {
+			//while(true) {
+				System.out.println(myname[1]);
+		//	}
+		}
+	}
+	
+}
